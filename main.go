ce49b2dc31a4cfe7e4d5068a0ee96baf730e8611
Scott Kidder <kidder.scott@gmail.com>
2015-07-10 13:31:18 -0600

    Pulls the latest stock price of Google and controls the heading of the Sphero

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -22,8 +26,49 @@ func main() {
 	driver := sphero.NewSpheroDriver(adaptor, "sphero")
 
 	work := func() {
+		var previousStockPrice float64
+
 		gobot.Every(3*time.Second, func() {
-			driver.Roll(30, uint16(gobot.Rand(360)))
+			// retrieve: opening price, last price
+			resp, err := http.Get("http://download.finance.yahoo.com/d/quotes.csv?s=GOOG&f=ol1")
+			defer resp.Body.Close()
+			if err == nil {
+				body, _ := ioutil.ReadAll(resp.Body)
+				quoteParts := strings.Split(string(body), ",")
+				if len(quoteParts) > 1 {
+					openingPrice, _ := strconv.ParseFloat(strings.TrimSpace(quoteParts[0]), 32)
+					lastPrice, _ := strconv.ParseFloat(strings.TrimSpace(quoteParts[1]), 32)
+
+					// set color based on status of stock
+					if lastPrice > openingPrice {
+						driver.SetRGB(0, 255, 0) //green
+					} else {
+						driver.SetRGB(255, 0, 0) //red
+					}
+					fmt.Printf("Last Price=%f, Opening Price=%f\n", lastPrice, openingPrice)
+
+					// calculate percentage change for the day
+					if previousStockPrice == 0.0 {
+						previousStockPrice = lastPrice
+					}
+					percentageChange := uint16(10000 * ((lastPrice - previousStockPrice) / previousStockPrice))
+					fmt.Printf("Percentage change: %d\n", percentageChange)
+
+					var heading uint16
+					if percentageChange < 0 {
+						heading = 360 + percentageChange
+					} else {
+						heading = percentageChange
+					}
+
+					fmt.Printf("Heading: %d\n", heading)
+					driver.Roll(100, heading)
+					time.Sleep(1 * time.Second)
+					driver.Stop()
+
+					previousStockPrice = lastPrice
+				}
+			}
 		})
 	}
 
@@ -46,8 +91,8 @@ func main() {
 	// Handle SIGINT and SIGTERM.
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	log.Println(<-ch)
+	fmt.Println(<-ch)
 
 	// Stop the service gracefully.
-	log.Println("Shutdown successful, exiting")
+	fmt.Println("Shutdown successful, exiting")
 }
